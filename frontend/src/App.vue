9ab4e4e12563ffd24d4551c87d828e5df1d6a719
jy-hwang <stratojy@gmail.com>
2025-05-22 14:05:23 +0900

    feat. Frontend - 로그인 폼 및 기능 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Header />
 
-  <Home />
+  <RouterView />
 
   <Footer />
 </template>
@@ -9,12 +9,10 @@
 <script>
 import Footer from "./components/Footer.vue";
 import Header from "./components/Header.vue";
-import Home from "./pages/Home.vue";
 
 export default {
   name: "App",
   components: {
-    Home,
     Footer,
     Header,
   },
