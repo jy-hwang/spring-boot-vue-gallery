9feb0ce61170838e2efd72e13239b90608831206
jy-hwang <stratojy@gmail.com>
2025-05-22 10:43:38 +0900

    feat. Backend - JPA로 데이터 조회하기
    1. 테이블 생성하고 Entity 객체랑 맞추기
    2. extends JpaRepository
    3. Controller 에서 Repository 직접 접근.(차후 변경)

---
+++ src/main/java/org/africalib/gallery/repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package org.africalib.gallery.repository;
+
+import org.africalib.gallery.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+
+}
