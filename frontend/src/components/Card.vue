e48d5c215c852dd4588e4533961fd87c8ab5056b
jy-hwang <stratojy@gmail.com>
2025-05-22 10:20:49 +0900

    feat. Backend <-> Frontend 통신확인
    1. 아주 간단한 형태의 api 를 하나 만들고
    2. 이를 axios.get 요청으로 받아와서
    3. Card Component 내부에 표현하기

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -17,8 +17,7 @@
 
     <div class="card-body">
       <p class="card-text">
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
+        {{ item }}
       </p>
       <div class="d-flex justify-content-between align-items-center">
         <div class="btn-group">
@@ -37,6 +36,9 @@
 <script>
 export default {
   name: "Card",
+  props: {
+    item: String,
+  },
 };
 </script>
 <style scoped></style>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -20,8 +20,8 @@
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col" v-for="i in 3" :key="i">
-            <Card />
+          <div class="col" v-for="(item, idx) in state.items" :key="idx">
+            <Card :item="item" />
           </div>
         </div>
       </div>
@@ -30,11 +30,24 @@
 </template>
 <script>
 import Card from "@/components/Card.vue";
+import axios from "axios";
+import { reactive } from "vue";
 
 export default {
   name: "Home",
   components: {
     Card,
+  },
+  setup() {
+    const state = reactive({
+      items: [],
+    });
+
+    axios.get("/api/items").then(({ data }) => {
+      state.items = data;
+    });
+
+    return { state };
   },
 };
 </script>
