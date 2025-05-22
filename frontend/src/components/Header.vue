17a9bd464d3984848373e337997eeab0f112484d
jy-hwang <stratojy@gmail.com>
2025-05-22 13:18:26 +0900

    chore. format line width
    1. 80 -> 200

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -6,10 +6,8 @@
           <div class="col-sm-8 col-md-7 py-4">
             <h4 class="text-white">About</h4>
             <p class="text-muted">
-              Add some information about the album below, the author, or any
-              other background context. Make it a few sentences long so folks
-              can pick up some informative tidbits. Then, link them off to some
-              social networking sites or contact information.
+              Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off
+              to some social networking sites or contact information.
             </p>
           </div>
           <div class="col-sm-4 offset-md-1 py-4">
@@ -37,24 +35,13 @@
             stroke-width="2"
             aria-hidden="true"
             class="me-2"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"
-            />
+            viewBox="0 0 24 24">
+            <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
             <circle cx="12" cy="13" r="4" />
           </svg>
           <strong>Album</strong>
         </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarHeader"
-          aria-controls="navbarHeader"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
       </div>
